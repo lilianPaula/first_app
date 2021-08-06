6d6a8fb2c750ebf22ebdaaf56015336a608ce206
lilianPaula <lilian.paula@alu.ufc.br>
2021-08-06 19:04:27 -0300

    ejs

---
+++ routes.js
@@ -0,0 +1,10 @@
+const express= require("express");
+const router = express.Router();
+const faker = require("faker");
+
+let db = require("./db");
+
+router.get("/",(req,res)=>{
+res.render('pages/home');
+res.send('Página de teste');
+});
