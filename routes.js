03a1e98f5627ecd405855e1396f1f03590826aec
lilianPaula <lilian.paula@alu.ufc.br>
2021-08-08 22:04:33 -0300

    consertando meus erros

--- routes.js
+++ routes.js
@@ -1,8 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const app = express();
-
-// const faker = require("faker");
+const faker = require("faker");
 
 // let db = require("./db");
 
